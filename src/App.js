21f6941497183078b14702329c33db0d2be0d683
uday chauhan <chauhanuday842@gmail.com>
2023-02-24 13:03:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+
+  function checkprime(n) {
+    var i, flag = true;
+
+    if (n < 2) return false
+
+    for (i = 2; i <= n - 1; i++) {
+      if (n % i === 0) {
+        flag = false;
+        break;
+      }
+
+    }
+    if (flag === true)
+      return true
+    return false
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className="para">30 Days of React </p>
+      <p className="para1">Number Generator </p>
+      <div className="maindiv">
+        { Array(32).fill(" ").map((e, i) => {
+          return (
+
+            <div className="mapArr" style={ { backgroundColor: checkprime(i) ? "red" : i % 2 === 0 ? "green" : "#CFC52A" } }>{ i }</div>
+          )
+        }) }
+      </div>
     </div>
   );
 }
